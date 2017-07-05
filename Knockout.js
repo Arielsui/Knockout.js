7fbac645f81f45b8de2c7b5d148abed51ed3e560
Arielsui <sevenup.sui@gmail.com>
2017-07-05 15:10:00 +1200

    Update Knockout.js

--- Knockout.js
+++ Knockout.js
@@ -1,1 +1,4 @@
-video tutorials for beginners:https://www.youtube.com/watch?v=-i_if99cEZU&list=PLzpzty4VRTPfbao0m8oADCmBD5BmWM4rT&index=1
+video tutorials for beginners:
+  http://learn.knockoutjs.com/
+  https://www.youtube.com/watch?v=-i_if99cEZU&list=PLzpzty4VRTPfbao0m8oADCmBD5BmWM4rT&index=1
+  
